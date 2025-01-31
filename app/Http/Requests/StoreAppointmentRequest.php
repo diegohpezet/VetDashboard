056334429166171c578a3f6a41e7386d234a239b
diegohpezet <diegohpezet@gmail.com>
2025-01-31 13:37:35 -0300

    appointment requests

---
+++ app/Http/Requests/StoreAppointmentRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
+
+class StoreAppointmentRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return Auth::check();
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'pet_id' => 'required|integer|exists:pets,id',
+            'date' => 'required|date|after_or_equal:now',
+            'reason' => 'nullable|string|max:255',
+            'status' => 'required|in:Scheduled,Completed,Cancelled'
+        ];
+    }
+}
