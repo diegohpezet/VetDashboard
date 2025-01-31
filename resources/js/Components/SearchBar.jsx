9ab7a4a450f5eddd42ef3044a531e12eb4c5dbe0
diegohpezet <diegohpezet@gmail.com>
2025-01-31 10:32:52 -0300

    Globalize search and pagination components

--- resources/js/Components/SearchBar.jsx
+++ resources/js/Components/SearchBar.jsx
@@ -1,20 +1,26 @@
 import { useForm } from "@inertiajs/react";
 
-export default function SearchBar({ initialSearch }) {
+/**
+ * Renders a searchbar for the specified resource
+ * @param { String } initialSearch - The initial search
+ * @param { String } resource - The resource route name to search
+ * @returns 
+ */
+export default function SearchBar({ initialSearch, resource }) {
   const { data, setData, get } = useForm({
     search: initialSearch || "",
   });
 
   const handleSearch = (e) => {
     e.preventDefault();
-    get(route("owners.index"), { preserveState: true, replace: true });
+    get(route(`${resource}.index`), { preserveState: true, replace: true });
   };
 
   return (
     <form onSubmit={handleSearch} className="mb-4 flex gap-2">
       <input
         type="text"
-        placeholder="Search owners..."
+        placeholder={`Search ${resource}...`}
         value={data.search}
         onChange={(e) => setData("search", e.target.value)}
         className="input input-bordered w-full"
