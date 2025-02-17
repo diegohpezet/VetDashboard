3897d6c5542af6f9bc25643f8809ba2448484a2e
diegohpezet <diegohpezet@gmail.com>
2025-02-16 21:25:26 -0300

    vaccination controller setup

---
+++ app/Http/Requests/StoreVaccinationRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreVaccinationRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'pet_id' => 'required|string|exists:pets,id',
+            'date' => 'required|date',
+            'name' => 'required|string|max:255',
+            'next_dose' => 'nullable|date'
+        ];
+    }
+}
