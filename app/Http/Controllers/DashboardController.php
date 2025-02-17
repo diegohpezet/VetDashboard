3b7e78faf9cfa46fe9b35ae71a8eb45005b1e50f
diegohpezet <diegohpezet@gmail.com>
2025-02-17 00:03:27 -0300

    include pet owner on dashboard appointment list

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -14,7 +14,8 @@ class DashboardController extends Controller
 {
   public function index()
   {
-    $todayAppointments = Appointment::whereDate('date', now()->format('Y-m-d'))->get();
+    $todayAppointments = Appointment::with('pet.owner')
+      ->whereDate('date', now()->format('Y-m-d'))->get();
 
     return Inertia::render('Dashboard', [
       'appointments' => $todayAppointments,
