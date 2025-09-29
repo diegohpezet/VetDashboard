131b75258055404ca83c783f1a312f3ef791a07e
diegohpezet <diegohpezet@gmail.com>
2025-09-29 16:27:18 -0300

    add new services button

--- resources/js/Pages/Services/Index.jsx
+++ resources/js/Pages/Services/Index.jsx
@@ -1,7 +1,7 @@
 import MainLayout from "@/Layouts/MainLayout";
 import SectionCard from "@/Components/SectionCard";
 import ServicesTable from "./Components/ServicesTable";
-import { Head } from "@inertiajs/react";
+import { Head, Link } from "@inertiajs/react";
 import { t } from "i18next";
 
 export default function ServicesIndex({ services }) {
@@ -10,7 +10,10 @@ export default function ServicesIndex({ services }) {
       <Head title="Services" />
 
       <SectionCard>
-        <h1 className="text-2xl font-bold">{t('services')}</h1>
+        <div className="md:flex justify-between items-center">
+          <h1 className="text-2xl font-bold">{t('services')}</h1>
+          <Link href={route('services.create')} className="btn btn-primary mb-3 font-bold text-lg">+ {t('services.create')}</Link>
+        </div>
 
         <div className="mt-3">
           <ServicesTable services={services} />
